30592c7977ed9e735f79a6108893f70cbe364c8f
Susana <susana.m.vik@gmail.com>
2021-04-05 10:29:14 -0400

    commit tree c874bd5297d49aefa94476337a083003a4a669b0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = 'bob'; 
+const leastFavoriteCustomer = 'value'
+
+funct upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+funct setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
+funct overwriteBestCustomer() {
+    bestCustomer = 'bob';
+}
+
+function changeLeastFavoriteeCustomer() {
+    leastFavoriteCustomer = 'billy'
+}
